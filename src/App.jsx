58297484a51e26046ffb766fc8128b12f151798f
Maria Mendonca <maria.mendonca156@gmail.com>
2023-06-07 10:17:23 +0530

    Added Lazy Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import Sidebar from './components/sidebar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/dashboard';
-import About from './pages/about';
-import Analytics from './pages/Analytics';
-import Products from './pages/Products';
-import AnalogClock from './pages/clocks/analogClock/analogClock';
-import DigitalClock from './pages/clocks/digitalClock/digitalClock';
+
+const Dashboard = lazy(() => import('./pages/dashboard'));
+const About = lazy(() => import('./pages/about'));
+const AnalogClock = lazy(() =>
+  import('./pages/clocks/analogClock/analogClock')
+);
+const DigitalClock = lazy(() =>
+  import('./pages/clocks/digitalClock/digitalClock')
+);
+const Analytics = lazy(() => import('./pages/Analytics'));
+const Products = lazy(() => import('./pages/Products'));
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Sidebar>
-          <Routes>
-            <Route path="/index.html" element={<Dashboard />} />
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/analogClock" element={<AnalogClock />} />
-            <Route path="/digitalClock" element={<DigitalClock />} />
-            <Route path="/weather" element={<Analytics />} />
-            <Route path="/products" element={<Products />} />
-          </Routes>
-        </Sidebar>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Sidebar>
+            <Routes>
+              <Route path="/index.html" element={<Dashboard />} />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/analogClock" element={<AnalogClock />} />
+              <Route path="/digitalClock" element={<DigitalClock />} />
+              <Route path="/weather" element={<Analytics />} />
+              <Route path="/products" element={<Products />} />
+            </Routes>
+          </Sidebar>
+        </Suspense>
       </BrowserRouter>
     </div>
   );
