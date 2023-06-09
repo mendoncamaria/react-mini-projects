c692d56726447ffe13f441baa7cdcec1597eaa32
Maria Mendonca <maria.mendonca156@gmail.com>
2023-06-09 10:37:31 +0530

    Added loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,25 @@ import React, { Suspense } from 'react';
 import Sidebar from './components/sidebar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { routes } from './common/path';
+import Loader from './components/loader/loader';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense
+          fallback={
+            <div>
+              <Loader />
+            </div>
+          }
+        >
           <Sidebar>
-          <Routes>
-          {routes.map((route, index) => (
-            <Route key={index} path={route.path} element={route.element} />
-          ))}
-        </Routes>
+            <Routes>
+              {routes.map((route, index) => (
+                <Route key={index} path={route.path} element={route.element} />
+              ))}
+            </Routes>
           </Sidebar>
         </Suspense>
       </BrowserRouter>
---
+++ src/components/loader/loader.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Loader() {
+  return (
+    <>
+      <div class="loadingio-spinner-spinner-fs07f4ree0v">
+        <div class="ldio-bhl6n9tb7r">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Loader;
